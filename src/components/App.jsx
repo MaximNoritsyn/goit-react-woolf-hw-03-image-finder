815a0dc7a0a049dfb7a74f32e0c5e798f8e061e8
Maxim Noritsyn <maximnoritsyn@gmail.com>
2024-04-13 15:39:10 +0300

    search bar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+import css from './app.module.css';
+import { SearchBar } from './search_bar/search_bar';
+import { Component } from 'react';
+
+export class App extends Component {
+
+  state = {
+    searchQuery: '',
+  };
+
+  submitSearch = (e) => {
+    e.preventDefault();
+    const searchQuery = e.target.elements.query.value.trim();
+    this.setState('searchQuery', searchQuery);
+  }
+
+  render() {
+    return <div className={css.app}>
+      <SearchBar submitSearch={this.submitSearch} />
     </div>
-  );
+  };
 };
